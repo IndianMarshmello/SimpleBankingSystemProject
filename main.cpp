2bf2d21e411a72c6c0c52bcf041efeb85b1eaf4e
Amaan Zaidi <amaan.hasan.zaidi@gmail.com>
2025-03-24 23:51:43 -0500

    v0.0.4

--- main.cpp
+++ main.cpp
@@ -1,42 +1,6 @@
 #include <iostream>
 #include <string>
-#include <vector>
-
-
-
-class Bank {
-    private:
-        std::vector<User> accounts;
-    public:
-        Bank() {};
-
-        /*
-            Other methods
-            Make methods for inserting a user into the vector, and for removing if needed.abort
-            To be determined, idk.
-        */
-
-        void insertUser(User T) {
-            accounts.emplace_back(T);
-        }
-
-        /*
-        
-        */
-
-        /*
-        void Signup() {
-            
-        }
-
-        void Login() {
-
-        }
-        */
-        //
-
-        ~Bank() {};
-};
+#include <map>
 
 class User {
     private:
@@ -60,35 +24,179 @@ class User {
         /*
         Other methods
         */
+
+        std::string getUsername() {
+            return username;
+        }
+
+        std::string getPassword() {
+            return password;
+        }
+
+        int checkBalance() {
+            return money;
+        }
+
+        int withdrawal(int n) {
+
+            money-=n;
+
+            return money;
+        }
+
+        int deposit(int n) {
+            money+=n;
+
+            return money;
+        }
     
         ~User() {};
 };
 
-void textPrompt() {
-    std::cout << "\nWelcome to the Login screen" << std::endl;
-    std::cout << "Here you will be prompted to login\nor if you do not have an account,\nyou may enter \"signup\" in the username field\n" << std::endl;
-}
+class Bank {
+    private:
+        std::map<std::string, User> accounts;
+    public:
+        Bank() {};
 
-int loginToBank() {
-    std::string tempname;
-    std::string temppass;
-    textPrompt();
-    std::cout << "Username: ";
-    std::cin >> tempname;
-    if (tempname == "signup") {
         /*
-            call signup function when I make it
+            Other methods
+            Make methods for inserting a user into the vector, and for removing if needed.abort
+            To be determined, idk.
         */
 
-        return 0;
-    }
-    std::cout << "\n\nPassword: ";
-    std::cin >> temppass;
+        void insertUser(User sigma) {
+            accounts.insert({sigma.getUsername(), sigma});
+        }
+
+        /*
+        
+        */
+
+        void loadBank() {
+            while(true) {
+                this->Signup();
+                User current = Login();
+                bool gay = true;
+                while(gay) {
+                    // going back in terminal menus will always be -1
+                    
+                    // need to make getters and setters like checking balance, and adding balance to account aka deposit and withdrawal.
+                    char option;
+                    std::cout << "Pick an option to navigate the menu (1,2,3,4)"
+                            << "\n1) check balance"
+                            << "\n2) withdrawal"
+                            << "\n3) deposit"
+                            << "\n4) logout"
+                            << "\n: " << std::endl;
+                    std::cin >> option;
+                    int amount;
+                    switch(option) {
+                        case '1':
+                            current.checkBalance();
+                            break;
+                        case '2':
+                            std::cout << "Withdrawal amount: ";
+                            std::cin >> amount;
+
+                            amount = current.withdrawal(amount);
+                            std::cout << "\n\nNew balance: " << amount;
+                            break;
+                        case '3':
+                            std::cout << "Depost amount: ";
+                            std::cin >> amount;
+
+                            amount = current.deposit(amount);
+                            std::cout << "\n\nNew balance: " << amount;
+                            break;
+                        case '4':
+                            gay = false;
+                            break;
+                        default:
+                            break;
+                    }
+
+                }
+            }
+        }
+
+        void Signup() {
+            
+            // if they already have an account and want to login, call Login()
+            // this->Login()
+            char YN;
+            std::cout << "Do you already have an account and need to login? Y/N: ";
+            std::cin >> YN;
+
+            if (YN == 'N') {
+                std::string name;
+                std::string pass;
+
+                std::cout << "\n\nUsername: ";
+                std::cin >> name;
+
+                std::cout << "\n\nPassword: ";
+                std::cin >> pass;
+
+                User user(name, pass);
+                this->insertUser(user);
+
+            } //else if (YN == 'Y') {
+
+            // } 
+
+        }
+
+        User Login() {
+            std::string name;
+            std::string pass;
+            std::cout << "What is your username?: ";
+            std::cin >> name;
+            std::cout << "\n\nWhat is your password?: ";
+            std::cin >> pass;
+            auto it = accounts.find(name);
+            User currentUser = it->second;
+            if (it == accounts.end()) {
+                std::cerr << "Username not found" << std::endl;
+                return User();  // Return a default user or handle error properly
+            }
+            if(currentUser.getPassword() != pass) {
+                std::cerr << "password incorrect" << std::endl;
+                return User();
+            }
+
+            return currentUser;
+        }
+        
+        //
+
+        ~Bank() {};
+};
 
 
 
-    return 0;
-}
+// void textPrompt() {
+//     std::cout << "\nWelcome to the Login screen" << std::endl;
+//     std::cout << "Here you will be prompted to login\nor if you do not have an account,\nyou may enter \"signup\" in the username field\n" << std::endl;
+// }
+
+// int loginToBank() {
+//     std::string tempname;
+//     std::string temppass;
+//     textPrompt();
+//     std::cout << "Username: ";
+//     std::cin >> tempname;
+//     if (tempname == "signup") {
+//         /*
+//             call signup function when I make it
+//         */
+//
+//         return 0;
+//     }
+//     std::cout << "\n\nPassword: ";
+//     std::cin >> temppass;
+//     return 0;
+// }
 
 
 
@@ -96,9 +204,9 @@ int loginToBank() {
 
 int main() {
 
-    loginToBank();
+    Bank theBank;
 
+    theBank.loadBank();
 
-    
     return 0;
 }
